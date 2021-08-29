f5fc75df55107e9b3f905ca8a45d6a0835dcaa4e
Caio Braga <caiobs.99@gmail.com>
2021-08-29 18:05:23 -0300

    minimax poc implementation

--- main.py
+++ main.py
@@ -2,11 +2,58 @@ import chess
 import os
 from random import sample
 
+piece_weights = {
+    chess.PAWN: 100,
+    chess.ROOK: 500,
+    chess.KNIGHT: 320,
+    chess.BISHOP: 330,
+    chess.QUEEN: 900,
+    chess.KING: 20000
+}
+
 def whose_turn():
   print("White's turn\n") if board.turn == chess.WHITE else print("Black's turn\n")
 
 def move(movement):
   board.push(movement) if movement in board.legal_moves else None
+
+def evaluate(board):
+  top_value = 0
+  for movement in board.legal_moves:
+    weight = piece_weights[board.piece_type_at(movement.from_square)]
+    if weight > top_value:
+      top_value = weight
+  return top_value
+
+def minimax_root(board, depth, maximizing_player):
+  best_move = None
+  best_value = -float('inf')
+
+  for movement in board.legal_moves:
+    move(movement)
+    value = max(best_value, minimax(board, depth - 1, not maximizing_player))
+    board.pop()
+    if value > best_value:
+      best_move = movement
+  return best_move
+
+def minimax(board, depth, maximizing_player):
+  if depth == 0 or board.is_game_over():
+    return evaluate(board)
+  if maximizing_player:
+    value = -float('inf')
+    for movement in board.legal_moves:
+      move(movement)
+      value = max(value, minimax(board, depth - 1, False))
+      board.pop()
+    return value
+  else:
+    value = float('inf')
+    for movement in board.legal_moves:
+      move(movement)
+      value = min(value, minimax(board, depth - 1, True))
+      board.pop()
+    return value
 
 board = chess.Board()
 
@@ -19,5 +66,6 @@ while not board.is_game_over():
     position = chess.Move.from_uci(pos)
     move(position)
   else:
-    position = sample(list(board.legal_moves), 1)[0]
+    # position = sample(list(board.legal_moves), 1)[0]
+    position = minimax_root(board, 2, True)
     move(position)
